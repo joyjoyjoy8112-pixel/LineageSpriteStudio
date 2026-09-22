50cca04ea913bd4b0280d23e16e34057ee6b3004
joyjoyjoy8112-pixel <joyjoyjoy8112@gmail.com>
2026-09-22 22:12:34 +0900

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace LineageSpriteStudio;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
